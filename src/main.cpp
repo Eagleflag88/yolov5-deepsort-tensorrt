a37b09ab51a26173310d502a23fa87a530c74825
Eagleflag88 <yijiang.xie@foxmail.com>
2022-05-04 20:10:28 +0800

    Add path for detection and sort engine

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-10 23:53:55
+ * @LastEditTime: 2022-01-12 22:35:28
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /yolov5-deepsort-tensorrt/src/main.cpp
+ */
 #include<iostream>
 #include "manager.hpp"
 #include <opencv2/opencv.hpp>
@@ -18,13 +26,15 @@ int main(){
 	map<int,vector<int>> personstate;
 	map<int,int> classidmap;
 	bool is_first = true;
-	char* yolo_engine = "";
-	char* sort_engine = "";
+	char* yolo_engine = "/work/tools/yolov5-deepsort-tensorrt/models/yolov5/yolov5s.engine";
+	char* sort_engine = "/work/tools/yolov5-deepsort-tensorrt/models/deepsort/yolosort/deepsort.engine";
 	float conf_thre = 0.4;
 	Trtyolosort yosort(yolo_engine,sort_engine);
 	VideoCapture capture;
 	cv::Mat frame;
-	frame = capture.open("");
+	// frame = capture.open("/work/tools/yolov5-deepsort-tensorrt/videos/demo2.avi");
+	frame = capture.open("/work/tools/FairMOT/videos/MOT16-03.mp4");
+	// frame = capture.open(0);
 	if (!capture.isOpened()){
 		std::cout<<"can not open"<<std::endl;
 		return -1 ;
@@ -44,6 +54,7 @@ int main(){
 		auto end = std::chrono::system_clock::now();
 		int delay_infer = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 		std::cout  << "delay_infer:" << delay_infer << "ms" << std::endl;
+		// yosort.showDetection(frame,det);
 		}
 		i++;
 	}
--- src/manager.cpp
+++ src/manager.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-10 23:53:55
+ * @LastEditTime: 2022-01-12 22:33:58
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /yolov5-deepsort-tensorrt/src/manager.cpp
+ */
 #include "manager.hpp"
 using std::vector;
 using namespace cv;
@@ -29,7 +37,7 @@ int Trtyolosort::TrtDetect(cv::Mat &frame,float &conf_thresh,std::vector<DetectB
 	// yolo detect
 	auto ret = yolov5_trt_detect(trt_engine, frame, conf_thresh,det);
 	DS->sort(frame,det);
-	//showDetection(frame,det);
+	showDetection(frame,det);
 	return 1 ;
 	
 }
